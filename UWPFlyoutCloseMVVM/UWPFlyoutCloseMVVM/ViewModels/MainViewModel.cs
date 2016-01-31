829535cea75a0fa1963665f652dd022e5bffbf96
Depechie <glenn.versweyveld@gmail.com>
2016-01-31 12:26:40 +0100

    Initial commit

---
+++ UWPFlyoutCloseMVVM/UWPFlyoutCloseMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace UWPFlyoutCloseMVVM.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private bool _isFlyoutClosed;
+        public bool IsFlyoutClosed
+        {
+            get { return _isFlyoutClosed; }
+            set
+            {
+                Set(() => IsFlyoutClosed, ref _isFlyoutClosed, value);
+                if (value)
+                    IsFlyoutClosed = false;
+            }
+        }
+
+        private RelayCommand _longCodeCommand;
+        public RelayCommand LongCodeCommand => _longCodeCommand ?? (_longCodeCommand = new RelayCommand(LongCode));
+
+        public MainViewModel(IMessenger messenger) : base(messenger)
+        {
+        }
+
+        public void LongCode()
+        {
+            IsFlyoutClosed = true;
+        }
+    }
+}
---
+++ UWPFlyoutCloseMVVM/UWPFlyoutCloseMVVM/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
+using Microsoft.Practices.ServiceLocation;
+
+namespace UWPFlyoutCloseMVVM.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private static void Register<T>(bool createImmediately = false) where T : class
+        {
+            SimpleIoc.Default.Register<T>(createImmediately);
+        }
+
+        internal static T Get<T>() where T : class
+        {
+            return SimpleIoc.Default.GetInstance<T>();
+        }
+
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<IMessenger, Messenger>();
+
+            Register<MainViewModel>();
+        }
+
+        public MainViewModel Main => Get<MainViewModel>();
+    }
+}
